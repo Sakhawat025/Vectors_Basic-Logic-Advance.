88e744714b506abcd71dba93cf6b85866e09f6fe
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2023-11-17 22:18:09 +0600

    Add files via upload

---
+++ Vector_Access_Elements.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector<int>num{1,2,3,4,5,6};
+
+    cout<< "Elements at Index 0: "<<num.at(0)<<endl;
+    cout<< "Elements at Index 2: "<<num.at(2)<<endl;
+    cout<< "Elements at Index 4: "<<num.at(4);
+
+    return 0;
+}
